a08fddf0e3633edb0d52af892dced8890913f167
djsegal <djsegal@wisc.edu>
2016-04-25 00:00:58 -0400

    Flesh out github.io website

---
+++ tests/dummy/app/routes/application.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  activate: function() {
+    Ember.run.scheduleOnce('afterRender', this, function() {
+      Ember.$('.button-collapse').sideNav();
+    })
+  },
+
+  actions: {
+    willTransition: function() {
+      if ( $('#js-nav-mobile-collapse').is(':visible') ) {
+        Ember.$('#js-nav-mobile-collapse').click();
+      }
+
+      return true;
+    },
+  },
+
+  renderTemplate: function() {
+
+    // Render default outlet
+
+    this.render();
+
+    // render extra outlets
+
+    this.render("navbar", {
+      outlet: "navbar",
+      into: "application" // important when using at root level
+    });
+
+    this.render("sidebar", {
+      outlet: "sidebar",
+      into: "application" // important when using at root level
+    });
+
+  },
+
+});
